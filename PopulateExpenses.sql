1dade66218f477df0445f4c50c03462e442318fd
Wikus Richards <WHOBAY@Gmail.com>
2012-07-12 21:46:52 +0200

    Updated populateExpenses File.

--- PopulateExpenses.sql
+++ PopulateExpenses.sql
@@ -1,3 +1,5 @@
-Insert into expenses values('Clothing','100256984', 1)
-Insert into expenses values('Food','785362569', 2)
-Insert into expenses values('Travel','256325148', 3)
+USE mem_db;
+
+Insert into expenses(Category, Account_Number, SMS_ID) values('Clothing','100256984', 1);
+Insert into expenses(Category, Account_Number, SMS_ID) values('Food','785362569', 2);
+Insert into expenses(Category, Account_Number, SMS_ID) values('Travel','256325148', 3);
